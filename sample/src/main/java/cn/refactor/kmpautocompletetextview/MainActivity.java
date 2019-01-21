788124c859ff1cbe2af71c554725456c3547122b
Bernhard Muller <bmwilhelm@gmail.com>
2019-01-21 19:32:34 +0200

    Updated gradle and converted support libraries to android x

--- sample/src/main/java/cn/refactor/kmpautocompletetextview/MainActivity.java
+++ sample/src/main/java/cn/refactor/kmpautocompletetextview/MainActivity.java
@@ -2,7 +2,7 @@ package cn.refactor.kmpautocompletetextview;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import java.util.ArrayList;
